79fcbf2d872c35f969204b6aaa7ee46c990cf4fd
Kanak <kkupgrader.fs@gmail.com>
2022-11-10 13:00:09 +0530

    service worker js

--- src/components/PageMetaData.tsx
+++ src/components/PageMetaData.tsx
@@ -46,8 +46,8 @@ export default function PageMetaData({ PageTitle, SiteName, PageDescription, Pag
             <link href='https://fonts.cdnfonts.com' rel='preconnect' />
             <link crossOrigin='' href='https://fonts.gstatic.com' rel='preconnect' />
 
-            <meta content='width=device-width, initial-scale=1' name='viewport' />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+            <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
             <link rel="icon" type="image/svg+xml" href="/kkupgrader.svg" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="manifest" href="/manifest.json" />
