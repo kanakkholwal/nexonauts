f1d0594e317b1717173435dae8f869ec53c094a8
Kanak <kkupgrader.fs@gmail.com>
2022-11-10 22:56:52 +0530

    Changed something

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  // swSrc: "./public/sw.js",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
 
-const nextConfig = {
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
-  env: {
-    ROOT: __dirname,
-  },
+  swcMinify: true,
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
-  pwa: {
-    dest: "public",
-    sw: "/sw.js",
-    swSrc: "./public/sw.js",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
+});
 
-  },
-}
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
 
 // module.exports = nextConfig
