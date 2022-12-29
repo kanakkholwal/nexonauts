72a3d3d673be599b52260cd29e1af4c3df8e0dca
kkupgrader <kkupgrader.fs@gmail.com>
2022-12-29 15:44:09 +0530

    Added Youtube Downloader

---
+++ pages/api/download/youtube.js
@@ -0,0 +1,21 @@
+import ytdl from "ytdl-core";
+
+export default async function YouTubeDownloader(req, res) {
+
+    if (req.method === 'POST') {
+
+        const { url } = req.body;
+        const output = await ytdl.getInfo(url);
+
+        if (output)
+            res.status(200).json({
+                videoDetails: output.videoDetails,
+                Downloadable: output.formats.sort((a, b) => a.mimeType < b.mimeType)
+            })
+        else
+            res.status(404).end("Error occurred");
+    }
+    else
+        return res.status(404);
+
+}
