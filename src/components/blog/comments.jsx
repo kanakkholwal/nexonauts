5159a0e5f72fde0db45db1e5a82d138850815d7c
Kanak <kkupgrader.fs@gmail.com>
2023-05-24 17:04:14 +0530

    comment section added

--- src/components/blog/comments.jsx
+++ src/components/blog/comments.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Input, TextArea, FormElement, Label } from "components/form-elements";
 import { timeAgo } from "lib/scripts";
 import Button from "components/buttons";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Image from "next/image";
 import useSWR from "swr";
 import axios from "axios";
@@ -15,7 +15,81 @@ const CommentSection = styled.section`
   margin-top: 20px;
   border-top: 1px solid rgba(var(--mute-rgb), 0.25);
 `;
+const AddReplyButton = styled(Button)`
+font-size:0.75rem;
+padding: 0.25rem 0.5rem;
+margin: 0.5rem 0;
+line-height:1;
+`;
+const ToggleReplyButton = styled(Button)`
+font-size:0.75rem;
+padding: 0.25rem 0.5rem;
+margin: 0.5rem 0;
+line-height:1;
+`;
+const CommentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 20px;
+  &:not(:last-child){
+    border-bottom: 1px solid rgba(var(--mute-rgb), 0.25);
+  }
+  padding-bottom:0.75rem;
+`;
+const CommentCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+`;
+const CommentHeader = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content:space-between;
+  gap: 0.5rem;
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+`;
+const CommentBody = styled.p`
+  width: 100%;
+  padding:0.75rem 0.5rem;
+  text-align: initial;
+  background:rgb(231 232 239);
+  border-radius:0.5rem;
+`;
+const RepliesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+  overflow: hidden;
+  margin-left:1rem;
+  border-left: 1px solid rgba(var(--mute-rgb), 0.25);
+  padding-left:0.75rem;
 
+    position: relative;
+    height: 0;
+    transition: height .5s cubic-bezier(0.4, 0, 0.2, 1);
+    width: 100%;
+    &.isOpen{
+            height: auto;
+    } 
+`;
+const ReplyForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+  overflow: hidden;
+    position: relative;
+    height: 0;
+    transition: height .5s cubic-bezier(0.4, 0, 0.2, 1);
+    width: 100%;
+    &.isOpen{
+            height: auto;
+    } 
+`;
 const fetchData = async (url, data) => {
   const response = await axios.post(url, data);
   return response.data;
@@ -58,7 +132,7 @@ export default function Comments({ post }) {
   }, [data]);
 
   return (
-    <CommentSection>
+    <CommentSection id="comments">
       {comments.enabled === false ? <h5>Comments are disabled for this post</h5> : <h5>Comments</h5>}
       {isLoading ? <h6>Loading...</h6> : null}
       {error ? <h6>Something went wrong</h6> : null}
@@ -111,7 +185,9 @@ function Comment({ comment, postId }) {
   const [replyEmail, setReplyEmail] = useState('');
   const [replyComment, setReplyComment] = useState('');
   const [showReplyForm, setShowReplyForm] = useState(false);
-
+  const [showReply, setShowReply] = useState(false);
+  const repliesRef = useRef(null);
+  const replyFormRef = useRef(null);
   const handleReply = async (event, commentId) => {
     event.preventDefault();
     const nestedComment = {
@@ -140,51 +216,87 @@ function Comment({ comment, postId }) {
   };
 
   return (
-    <div>
-      <div className="d-flex justify-content-start align-items-center" id={comment._id} ariaLabelledBy={comment.parentComment ? comment.parentComment : "none"}>
-      <div>
-        <h6>{comment.name}</h6>
-        <p>{comment.name}</p>
-      </div>
-      <div>
-        <span>{timeAgo(new Date(comment.createdAt))}</span>
-      </div>
-      </div>
-      <Button size="sm" level="true" onClick={() => setShowReplyForm(!showReplyForm)}>
-        Reply
-      </Button>
-      {showReplyForm && (
-        <form onSubmit={(event) => handleReply(event, comment._id)}>
-          <Input
-            type="text"
-            placeholder="Name"
-            value={replyName}
-            onChange={(e) => setReplyName(e.target.value)}
-            required
-          />
-          <Input
-            type="email"
-            placeholder="Email"
-            value={replyEmail}
-            onChange={(e) => setReplyEmail(e.target.value)}
-            required
-          />
-          <TextArea
-            placeholder="Your Reply"
-            value={replyComment}
-            onChange={(e) => setReplyComment(e.target.value)}
-            required
-          ></TextArea>
+    <>
+      <CommentWrapper id={comment._id} ariaLabelledBy={comment.parentComment ? comment.parentComment : "none"}>
+        <CommentCard>
+          <CommentHeader>
+            <div>
+              <h6>{comment.name}</h6>
+            </div>
+            <div>
+              <span>{timeAgo(new Date(comment.createdAt))}</span>
+            </div>
+          </CommentHeader>
+          <CommentBody>{comment.comment}</CommentBody>
+        </CommentCard>
+        <div className="d-flex align-items-center justify-content-start g-3">
+          <AddReplyButton size="sm" level="true" onClick={() => setShowReplyForm(!showReplyForm)}>
+            Reply
+          </AddReplyButton>
+          {comment.replies.length > 0 ? (
+            <ToggleReplyButton size="sm" level="true" onClick={() => setShowReply(!showReply)}>
+              {showReply ? 'Hide Replies' : `Show ${comment.replies.length} Replies`}
+            </ToggleReplyButton>
+          ) : null}
+        </div>
+
+        <ReplyForm onSubmit={(event) => handleReply(event, comment._id)}
+          ref={replyFormRef}
+          className={`Collapse ${showReplyForm ? "isOpen" : ""}`}
+          style={
+            showReplyForm
+              ? { height: replyFormRef.current?.scrollHeight + "px" }
+              : { height: "0px" }
+          }>
+
+          <FormElement>
+            <Label htmlFor="name">Name</Label>
+            <Input
+              type="text"
+              placeholder="Name"
+              value={replyName}
+              onChange={(e) => setReplyName(e.target.value)}
+              required
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              placeholder="Email"
+              value={replyEmail}
+              onChange={(e) => setReplyEmail(e.target.value)}
+              required
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor="name">Your Reply</Label>
+            <TextArea
+              placeholder="Your Reply"
+              value={replyComment}
+              onChange={(e) => setReplyComment(e.target.value)}
+              required
+            ></TextArea>
+          </FormElement>
+
           <Button type="submit">Submit Reply</Button>
-        </form>
-      )}
-      {comment.replies.length > 0 && (
-        <div style={{ marginLeft: '20px' }}>
+        </ReplyForm>
+
+        <RepliesWrapper
+          ref={repliesRef}
+
+          className={`Collapse ${showReply ? "isOpen" : ""}`}
+          style={
+            showReply
+              ? { height: repliesRef.current?.scrollHeight + "px" }
+              : { height: "0px" }
+          }>
           {comment.replies.map((reply) => (
             <Comment key={reply._id} comment={reply} postId={postId} />
           ))}
-        </div>
-      )}
-    </div>
+        </RepliesWrapper>
+      </CommentWrapper>
+
+    </>
   );
 }
