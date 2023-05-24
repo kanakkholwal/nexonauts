625d9481ef1751776c9988631a2ea8450a828b0c
Kanak <kkupgrader.fs@gmail.com>
2023-05-24 15:17:13 +0530

    comment logic done with apis

--- pages/api/posts/[slug]/comments/index.js
+++ pages/api/posts/[slug]/comments/index.js
@@ -4,42 +4,41 @@ import Post, { Comment } from "models/post";
 import dbConnect from "lib/dbConnect";
 import nextConnect from 'next-connect';
 
+export default nextConnect(handler).post(async (req, res) => {
+  try {
+    await dbConnect();
 
-export default nextConnect(handler)
-    .post(async (req, res) => {
-        try {
-            await dbConnect();
+    const { slug: postId } = req.query;
+    const { name, email, comment } = req.body;
 
+    // Find the post by ID
+    const post = await Post.findById(postId)
 
-            const { slug: postId } = req.query;
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
 
-            const { name, email, comment: commentBody } = req.body;
+    // Create a new comment
+    const newComment = new Comment({
+      name,
+      email,
+      comment,
+      post: postId,
+    });
 
-            // Find the post by ID
-            const post = await Post.findById(postId).populate('comments.items')
-            if (!post) {
-                return res.status(404).json({ message: 'Post not found' });
-            }
-            const newComment = {
-                name,
-                email,
-                comment: commentBody,
-                children: [],
-                post: postId,
-            };
-            const comment = await Comment.create(newComment)
-            if (!comment) {
-                return res.status(500).json({ message: 'Unable to create a new comment!' });
-            }
-            // Update the post
-            post.comments.items.push(comment);
-            post.comments.numberOfComments = post.comments.items.length;
-            await post.save();
-            return res.status(200).json({ message: 'Comment created successfully!', comment: newComment })
-        }
-        catch (err) {
-            console.error('Error adding comment:', err);
-            res.status(500).json({ message: err.message ?? 'Server error' });
-        }
+    // Save the comment
+    await newComment.save();
 
-    })
+    // Add the comment to the post's comments array
+    // post.comments.rootItems.push(newComment);
+    post.comments.numberOfComments += 1;
+
+    // Save the updated post
+    await post.save();
+
+    return res.status(201).json(newComment);
+  } catch (err) {
+    console.error('Error adding comment:', err);
+    return res.status(500).json({ message: err.message ?? 'Server error' });
+  }
+});
