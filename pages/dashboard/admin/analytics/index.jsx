02a06e88456e406071fa01b70f0e55540f9d4d6a
Kanak <kkupgrader.fs@gmail.com>
2023-06-03 01:37:45 +0530

    analytics page done for now untill user boost

--- pages/dashboard/admin/analytics/index.jsx
+++ pages/dashboard/admin/analytics/index.jsx
@@ -1,11 +1,17 @@
 import { getSession } from "next-auth/react";
 import DashboardPage from "components/dashboard-page";
+import { AnalyticCard, DashCard, Icon } from "components/dashboard-page/elements";
 import Head from "next/head";
 import State from 'components/state';
 import { useEffect, useState } from 'react';
 import { Card, CardBody, CardHeader, CardTitle } from 'components/Card';
+import { intervalWise, authWise, pageWise,getPopularPages, ConversionRate, getAnalyticsByAction, getAnalytics } from 'lib/analytics';
 import useSWR from 'swr';
 import axios from 'axios';
+import { AiOutlineEye } from "react-icons/ai";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { HiOutlineRefresh } from "react-icons/hi";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
 import dynamic from "next/dynamic";
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
@@ -24,25 +30,70 @@ export default function Stats({ user }) {
         <title>Analytics</title>
       </Head>
       <DashboardPage user={user}>
-        <State
+        {!data && (<State
           loader={{ type: "indeterminate", show: isLoading }}
           alert={{
             type: error ? "danger" : "success",
-            message: error ? error.message : "Notifications loaded successfully",
+            message: error ? error.message : "Analytics fetched successfully",
             show: error ? true : false
-          }}
-        />
+          }} />)}
+
+        <div className="d-flex justify-content-start align-items-start g-3 mb-3 flex-wrap">
+          <DashCard width={"16rem"}>
+            <div>
+              <span>Total Activities</span>
+              <h2>{data ? getAnalytics(data.pages).length : 0}</h2>
+            </div>
+            <Icon>
+              <TbBrandGoogleAnalytics />
+            </Icon>
+          </DashCard>
+          <DashCard width={"16rem"}>
+            <div>
+              <span>Total Views</span>
+              <h2>{data ? getAnalyticsByAction(data.pages, "view").length : 0}</h2>
+            </div>
+            <Icon>
+              <AiOutlineEye />
+            </Icon>
+          </DashCard>
+          <DashCard width={"16rem"}>
+            <div>
+              <span>Total Shares</span>
+              <h2>{data ? getAnalyticsByAction(data.pages, "share").length : 0}</h2>
+            </div>
+            <Icon>
+              <IoShareSocialOutline />
+            </Icon>
+          </DashCard>
+          <DashCard width={"16rem"}>
+            <div>
+              <span>Conversion</span>
+              <h2>{data ?
+                parseFloat(ConversionRate(data.pages, "week").currentConversionRate - ConversionRate(data.pages, "week").pastConversionRate >= 0)
+                  ? "+" + parseFloat(ConversionRate(data.pages, "week").currentConversionRate - ConversionRate(data.pages, "week").pastConversionRate)
+                  :
+                  " - " + parseFloat(ConversionRate(data.pages, "week").currentConversionRate - ConversionRate(data.pages, "week").pastConversionRate) : 0.0}</h2>
+            </div>
+            <Icon>
+              <HiOutlineRefresh />
+            </Icon>
+          </DashCard>
+        </div>
         <div className="d-flex justify-content-around align-items-start g-3 flex-wrap">
 
 
-        <HandlePieChart data={data?.pages} />
-       </div>
-       </DashboardPage>
+          {/* <IntervalWise data={data?.pages} /> */}
+          <PageTypeWise data={data?.pages} />
+          <SessionTypeWise data={data?.pages} />
+
+
+        </div>
+      </DashboardPage>
     </>
   );
 }
-
-function HandlePieChart({ data }) {
+function IntervalWise({ data }) {
   const [state, setState] = useState(null);
 
   useEffect(() => {
@@ -53,39 +104,74 @@ function HandlePieChart({ data }) {
   }, [data]);
 
   const processData = (pages) => {
-    const series = [];
-    const categories = [];
+    const data = intervalWise(pages, 'hourly')
+    console.log(data);
 
-    pages.forEach((page) => {
-      const pageType = page.type;
-      const pageAnalytics = page.analytics;
-      const actionCount = {
-        view: 0,
-        share: 0,
-      };
 
-      pageAnalytics.forEach((analytics) => {
-        actionCount[analytics.action]++;
-      });
-
-      series.push(pageAnalytics.length);
-
-      categories.push(pageType);
-    });
 
     return {
-      series,
+      series: data.series,
+      options: {
+        chart: {
+          type: 'line',
+          zoom: {
+            enabled: false
+          },
+          fontSize: 12, // Ensure this property is defined
+        },
+        xaxis: data.xaxis,
+        title: {
+          text: 'Visitor Trends by ' + 'hourly',
+          align: 'left'
+        },
+        dataLabels: {
+          enabled: false
+        }
+
+      },
+    };
+  };
+
+  return (
+    <AnalyticCard>
+      {state && (
+        <Chart
+          options={state.options}
+          series={state.series}
+          // type="line"
+          height={480}
+        />
+      )}
+    </AnalyticCard>
+  );
+}
+function PageTypeWise({ data }) {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      const processedData = processData(data);
+      setState(processedData);
+    }
+  }, [data]);
+
+  const processData = (pages) => {
+    const data = pageWise(pages) || { series: [], labels: [] };
+    console.log(data);
+
+    return {
+      series: data.series,
       options: {
         chart: {
           type: 'pie',
         },
-        labels: categories,
+        labels: data.labels,
         responsive: [{
-          breakpoint: 480,
+          breakpoint: 400,
           options: {
             chart: {
               width: 400,
-              height: 450
+              height: 480,
 
             },
             legend: {
@@ -99,17 +185,85 @@ function HandlePieChart({ data }) {
 
   return (
     <Card>
+      <CardHeader>
+        <CardTitle>Page Views</CardTitle>
+        {/* <Select onChange={e => setFilter(e.target.value)} value={filter}/> */}
+      </CardHeader>
       {state && (
         <Chart
           options={state.options}
           series={state.series}
           type="pie"
-          width={380}
+          height={480}
         />
       )}
     </Card>
   );
 }
+function SessionTypeWise({ data }) {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      const processedData = processData(data);
+      setState(processedData);
+    }
+  }, [data]);
+
+  const processData = (pages) => {
+    const data = authWise(pages)
+    console.log(data);
+
+  
+    return {
+      series: data.series,
+      options: {
+        chart: {
+          type: 'donut',
+        },
+        plotOptions: {
+          pie: {
+            startAngle: -90,
+            endAngle: 90,
+            offsetY: 10
+          }
+        },
+        grid: {
+          padding: {
+            bottom: -80
+          }
+        },
+        labels: data.labels,
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+
+      },
+    };
+  };
+
+  return (
+    <AnalyticCard>
+      {state && (
+        <Chart
+          type="donut"
+          options={state.options}
+          series={state.series}
+          height={480}
+        />
+      )}
+    </AnalyticCard>
+  );
+}
+
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
--- src/components/dashboard-page/elements.jsx
+++ src/components/dashboard-page/elements.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Card } from "components/Card"
 
 export const Header = styled.div`
 padding:0.75rem;
@@ -14,4 +15,62 @@ font-size:100%;
 svg{
     margin-inline:0.5rem;
 }
+`;
+
+export const DashCard = styled(Card)`
+max-width:${({width}) => width ? width : '350px'};
+flex:1 1 auto;
+display:flex;
+align-items:center;
+justify-content:space-between;
+flex-direction:row;
+gap:0.75rem;
+padding:1.5rem 1.5rem;
+background:var(--card-bg);
+${'' /* box-shadow:var(--card-shadow-single); */}
+span{
+    font-size:1rem;
+    font-weight:500;
+    letter-spacing:0.025rem;
+    margin:0;
+    color:rgba(var(--text-rgb),0.6);
+}
+`;
+export const Icon = styled.div`
+color:rgba(var(--text-rgb),0.9);
+display:inline-flex;
+align-items:center;
+justify-content:center;
+border-radius:0.5rem;
+aspect-ratio:1;
+max-width:120px;
+max-height:60px;
+svg{
+    font-size:48px;
+    margin:auto;
+    aspect-ratio:1;
+}
+`;
+
+export const AnalyticCard = styled(Card)`
+width:max-content;
+max-width:100%;
+flex:1 1 auto;
+--padding:1.5rem;
+padding:var(--padding);
+
+${({maxWidth}) =>{
+    if(maxWidth){
+        return `
+        max-width:calc(${maxWidth} + var(--padding) * 2);
+        `
+    }
+}}
+${({maxHeight}) =>{
+    if(maxHeight){
+        return `
+        max-height:calc(${maxHeight} + var(--padding) * 2);
+        `
+    }
+}}
 `;
