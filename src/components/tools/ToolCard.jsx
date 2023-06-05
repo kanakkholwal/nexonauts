647808f8a9de6e0b18639ecf349148ce6d9a0cd6
Kanak <kkupgrader.fs@gmail.com>
2023-06-05 15:54:53 +0530

    minor ux fix

--- src/components/tools/ToolCard.jsx
+++ src/components/tools/ToolCard.jsx
@@ -3,16 +3,14 @@ import Badge from "components/topography/badge";
 import { RiShareCircleFill } from "react-icons/ri";
 import Link from "next/link";
 
-export default function ToolCard({ title, description, path, category, online, ...props }) {
+export default function ToolCard({ title, description, path, category, online,index, ...props }) {
 
     return (
         <Card  {...props}>
             <CardBody className="d-flex flex-column justify-content-start">
                 <CardTitle>{title}</CardTitle>
                 <div>
-                    <Badge className="mb-2" as="small"  nature={["danger","theme","warning","info","secondary"][Math.floor(Math.random() * 5)]} pill>{category}</Badge>
-                    <Badge nature={(online ? "success" : "danger")} className={"mb-2 ms-2 "} pill title={online ? " Tool is working Fine :)" : "Tool has Some Issues ;)"}>{online ? "On" : "Off"}</Badge>
-
+                    <Badge className="mb-2" as="small"  nature={["success","theme","warning","info","secondary"][index > 5 ? index % 5 : index]} pill>{category}</Badge>
                 </div>
                 <CardDescription>
                     {description}
