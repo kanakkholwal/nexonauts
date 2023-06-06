cbfe16868dd86a99b83cffc22445e009c6dd2fce
Kanak <kkupgrader.fs@gmail.com>
2023-06-06 12:15:32 +0530

    fixes 4

--- pages/api/posts/[slug]/index.js
+++ pages/api/posts/[slug]/index.js
@@ -1,34 +1,35 @@
 import handler from 'lib/handler';
-import User from "models/user";
-import Page from "models/page";
-import Post from "models/post";
-import dbConnect from "lib/dbConnect";
+import User from 'models/user';
+import Page from 'models/page';
+import Post from 'models/post';
+import dbConnect from 'lib/dbConnect';
 import nextConnect from 'next-connect';
 
+export default nextConnect(handler).post(async (req, res) => {
+  try {
+    await dbConnect();
 
-export default nextConnect(handler)
-    .post(async (req, res) => {
-        try {
-            await dbConnect();
+    const { slug } = req.query;
 
+    const existingPost = await Post.findOne({ slug: slug })
+      .select('+content')
+      .populate('author.user', 'name profileURL')
+      .populate('analytics')
+      .exec();
 
-            const { slug } = req.query;
+    if (!existingPost) {
+      return res.status(404).json({ message: 'Post not found!' });
+    }
 
-            const existingPost = await Post.findOne({ slug: slug }).select('+content').populate('author.user', 'name profileURl').populate('analytics').exec();
-            if (!existingPost)
-                return res.status(404).json({ message: 'Post not found!' })
-            if (existingPost.state !== "published") {
-                return res.status(404).json({ message: 'Post not published!' })
-            }
+    if (existingPost.state !== 'published') {
+      return res.status(404).json({ message: 'Post not published!' });
+    }
 
-
-            return res.status(200).json({ message: 'Post Fetched Successfully!', post: existingPost })
-        }
-        catch (err) {
-            console.log(err);
-            return res.status(500).json({
-                message: err.message || " Something went wrong",
-            })
-        }
-
-    })
+    return res.status(200).json({ message: 'Post Fetched Successfully!', post: existingPost });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: err.message || 'Something went wrong',
+    });
+  }
+});
