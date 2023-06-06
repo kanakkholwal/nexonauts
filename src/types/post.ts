d50b1d6b06c84809a898f5123b080ff0c6359852
Kanak <kkupgrader.fs@gmail.com>
2023-06-06 12:35:58 +0530

    fixes 5

---
+++ src/types/post.ts
@@ -0,0 +1,50 @@
+export type Post = {
+    metadata?: {
+      keywords?: string[];
+      image?: string;
+    };
+    author: {
+      name: string;
+      profileURL: string;
+      user?: {
+        _id?: string;
+        name?: string;
+        profileURL?: string;
+      };
+    };
+    comments: {
+      enabled: boolean;
+    };
+    _id: string;
+    title: string;
+    content?: {
+      time?: number | Date;
+      blocks?: ContentBlock[];
+      version?: string;
+    };
+    slug: string;
+    labels: string[];
+    image: string;
+    state: string;
+    analytics: {
+      _id: string;
+      title: string;
+      slug: string;
+      type: string;
+      analytics: any[]; // You can replace `any` with a more specific type if known
+      __v: number;
+    };
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    __v: number;
+    description: string;
+  }
+  interface ContentBlock {
+    id: string;
+    type: string;
+    data: {
+      text: string;
+      level?: number;
+    };
+  }
