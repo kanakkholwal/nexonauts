894e0e98ff3bb5bed704144eba4e14c7b928e884
Kanak <kkupgrader.fs@gmail.com>
2023-06-06 11:48:59 +0530

    fixes

--- pages/api/posts/all.js
+++ pages/api/posts/all.js
@@ -1,68 +1,63 @@
 import handler from 'lib/handler';
-import User from "models/user";
-import Post from "models/post";
-import Page from "models/page";
-import dbConnect from "lib/dbConnect";
+import User from 'models/user';
+import Post from 'models/post';
+import Page from 'models/page';
+import dbConnect from 'lib/dbConnect';
 import nextConnect from 'next-connect';
 
-export default nextConnect(handler)
-  .get(async (req, res) => {
-    try {
-      await dbConnect();
+export default nextConnect(handler).get(async (req, res) => {
+  try {
+    await dbConnect();
 
-      const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
-      const limit = parseInt(req.query.limit) || 10; // Default to 10 posts per page
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 posts per page
+    const skip = (page - 1) * limit;
 
-      const skip = (page - 1) * limit;
+    const [posts, count] = await Promise.all([
+      Post.find({ state: 'published' })
+        .populate('author.user', 'name profileURL')
+        .select('title description slug labels image author createdAt publishedAt comments')
+        .populate('analytics')
+        .skip(skip)
+        .limit(limit)
+        .exec(),
+      Post.countDocuments({ state: 'published' })
+    ]);
 
-      const postsPromise = await Post.find({ state: 'published' })
-      .populate('author.user', 'name profileURl')
-      .select('title description slug labels image author createdAt publishedAt comments')
-      .populate('author.user', 'name profileURl')
-      .populate('analytics').exec()
-      .skip(skip)
-      .limit(limit)
+    const totalPages = Math.ceil(count / limit);
 
-      // .sort({ createdAt: -1 }) // Sort posts by creation date (descending)
+    return res.status(200).json({
+      message: 'Posts Fetched Successfully!',
+      currentPage: page,
+      totalPages: totalPages,
+      posts: posts,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: err.message || 'Something went wrong',
+    });
+  }
+}).post(async (req, res) => {
+  try {
+    await dbConnect();
 
-      const countPromise = await Post.countDocuments({ state: 'published' });
+    const posts = await Post.find({ state: 'published' })
+      .sort({ createdAt: -1 })
+      .populate('author.user', 'name profileURL')
+      .select('content title description slug labels image author createdAt publishedAt comments')
+      .populate('analytics')
+      .exec();
 
-      const [posts, count] = await Promise.all([postsPromise, countPromise]);
-
-      const totalPages = Math.ceil(count / limit);
-
-      return res.status(200).json({
-        message: 'Posts Fetched Successfully!',
-        currentPage: page,
-        totalPages: totalPages,
-        posts: posts,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: err.message || "Something went wrong",
-      });
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      await dbConnect();
-
-      const posts = await Post.find({ state: 'published' })
-        .sort({ createdAt: -1 })
-        .populate('author.user', 'name profileURl')
-        .select('content title description slug labels image author createdAt publishedAt comments')
-        .populate('analytics').exec();
-
-      return res.status(200).json({
-        message: 'Posts Fetched Successfully!',
-        noOfPosts: posts.length,
-        posts: posts,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: err.message || "Something went wrong",
-      });
-    }
-  });
+    return res.status(200).json({
+      message: 'Posts Fetched Successfully!',
+      noOfPosts: posts.length,
+      posts: posts,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: err.message || 'Something went wrong',
+    });
+  }
+});
