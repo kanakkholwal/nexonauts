d2ec0e627e174a2aee1aa89f886e324e300ba363
Kanak <kkupgrader.fs@gmail.com>
2023-08-07 18:43:49 +0530

    routes

---
+++ pages/directory/[id].tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+import axios from 'axios';
+import { useSession } from "next-auth/react";
+
+
+export async function getStaticPaths() {
+    // Return a list of possible value for toolName
+    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/public-tools/ssr`,{
+        headers:{
+            "Content-Type":"application/json",
+            "x-authorization":`Bearer ${process.env.NEXT_AUTH_SECRET}`
+        }
+    })
+
+    const {tools}  =  data;
+    const paths = tools.map(({ slug }) => {
+        return {
+            params: {
+                id: slug,
+            }
+        };
+    }) || [];
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    // Fetch necessary data for the tool using params.appName
+    try {
+
+        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/public-tools/ssr`,{
+            slug:params.id
+        },{
+            headers:{
+                "Content-Type":"application/json",
+                "x-authorization":`Bearer ${process.env.NEXT_AUTH_SECRET}`
+            }
+        })
+        const {tool}  =  data;
+
+        if (!tool) {
+            return {
+                notFound: true,
+                revalidate: 60,
+                props: {
+                    tool: null,
+                }
+            };
+        }
+        return {
+            props: {
+                tool
+            },
+            revalidate: 60,
+
+        }
+    } catch (error) {
+        console.log("Error during page generation using slug:", error);
+        return {
+            notFound: true,
+            props: {
+                tool: null,
+            },
+            revalidate: 60,
+        };
+    }
+
+}
+
+export default function Tool({tool}){
+
+
+    if(!tool) return null;
+
+    return <>
+        {/*  */}
+        {tool._id}
+    </>
+}
