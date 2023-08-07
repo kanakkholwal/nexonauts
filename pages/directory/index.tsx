2a31719f49c898660e3fc077a891785a4b79c52e
Kanak <kkupgrader.fs@gmail.com>
2023-08-07 18:23:22 +0530

    basic directory page done

--- pages/directory/index.tsx
+++ pages/directory/index.tsx
@@ -13,16 +13,17 @@ import {
 
 
 } from "src/layouts/directory-page";
-import {Wobble} from "components/Loader"
+import { Wobble } from "components/Loader"
 
-import {TbSearch,TbExternalLink,TbArrowBigRightLine} from "react-icons/tb";
+import { TbSearch, TbExternalLink, TbArrowBigRightLine } from "react-icons/tb";
 import Link from "next/link";
 
-import { Input,Select } from "components/form-elements";
+import { Input, Select, CheckBox, Label, FormElement } from "components/form-elements";
 
 
 import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const SocialMedia = [
     {
@@ -48,40 +49,66 @@ const SocialMedia = [
 ]
 
 export default function AiDirectory({
-    tools : defaultTools,
+    tools: defaultTools,
     categories
 }) {
 
     const [tools, setTools] = useState<PublicToolType[]>(defaultTools);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
     const [settings, setSettings] = useState({
-        search: "",
-        pricing: "Default",
-        category: "Default",
+        query: "",
+        page: 0,
+        limit: 10,
     });
-    console.log(categories);
+    const [filter, setFilter] = useState<{
+        pricing_type: string;
+        categories: string[];
+    }>({
+        pricing_type: "Default",
+        categories: [],
+    })
 
-    // useEffect(() =>{
-    //     axios.post("/api/public-tools/all",{},{
-    //         headers:{
-    //             "Content-Type": "application/json"
-    //         }
-    //     })
-    //     .then(res =>{
-    //         setTools(res.data);
-    //         setLoading(false);
-    //     }
-    //     )
-    //     .catch(err =>{
-    //         setError(true);
-    //         setLoading(false);
-    //     }
-    //     )
+    // search with debounce function
+    const debounce = (func, delay) => {
+        let debounceTimer
+        return function () {
+            const context = this
+            const args = arguments
+            clearTimeout(debounceTimer)
+            debounceTimer
+                = setTimeout(() => func.apply(context, args), delay)
+        }
+    }
 
-    // },[])
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        setSettings({
+            ...settings,
+            query: e.target.value
+        })
+        if(settings.query === ""){
+            setTools(defaultTools);
+            setLoading(false);
+            return;
+        }
 
-  
+        setLoading(true)
+        await axios.post(`/api/public-tools/search?query=${settings.query}&page=${settings.page}&limit=${settings.limit}`)
+            .then((res) => {
+                console.log(res.data)
+                setTools(res.data.tools)
+            })
+            .catch((err) => {
+                setError(err)
+                console.log(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
+
 
     return (
         <>
@@ -91,24 +118,24 @@ export default function AiDirectory({
             <DirectoryPageContainer>
                 <DirectoryPageNavBar>
                     <Link href="/directory" className="Title">
-                     AI Directory
+                        AI Directory
                     </Link>
                     <div className="LinkList">
                         <Link href="/">
-                           Home
+                            Home
                         </Link>
                         <Link href="/apps">
                             Services
                         </Link>
                         <Link href="/tools">
-                           Tools
+                            Tools
                         </Link>
                         <Link href="/blog">
-                           Blog
+                            Blog
                         </Link>
                     </div>
                     <Link href="/submit" className="Submit">
-                       Submit
+                        Submit
                     </Link>
 
                 </DirectoryPageNavBar>
@@ -130,80 +157,150 @@ export default function AiDirectory({
                 </DirectoryPageHero>
                 <DirectoryPageSearchContainer>
                     <div className="SearchBar">
-                        <Input type="search" 
+                        <Input type="search"
                             level={true}
-                         placeholder="Search for a tool" 
-                         lg={true}
-                         />
-                        <button className="SearchButton"><TbSearch/></button>
+                            placeholder="Search for a tool"
+                            lg={true}
+                            onChange={debounce(handleSearch, 500)}
+                        />
+                        <button className="SearchButton"><TbSearch /></button>
                     </div>
                     <Select
                         label="Pricing Type"
-                        value={"Default"}
+                        value={filter.pricing_type}
                         options={[
                             {
                                 label: "Default",
                                 value: "Default"
                             },
-                            
+                            {
+                                label: "Free",
+                                value: "Free"
+                            },
+                            {
+                                label: "Paid",
+                                value: "Paid"
+                            },
+                            {
+                                label: "Freemium",
+                                value: "Freemium"
+                            },
+                            {
+                                label: "Open Source",
+                                value: "open-source"
+                            },
+
+
                         ]}
                         lg={true}
-                        onChange={option =>{
-                            console.log(option)
+
+                        onChange={option => {
+                            setFilter({
+                                ...filter,
+                                pricing_type: option.value
+                            })
                         }}
                     />
                 </DirectoryPageSearchContainer>
 
-<div className="d-flex justify-content-between align-items-stretch g-3">
+                <div className="d-flex justify-content-between align-items-stretch g-3">
 
-                <DirectoryPageSearchResults>
-                    {tools.map((tool, index) => {
+                    <DirectoryPageSearchResults>
+                        {loading ? <Wobble /> : null}
 
-                    return (<div className="SearchResult" key={tool._id}>
+                        {tools.filter(tool => {
+                            let isValid = true;
+                            // if(settings.search === ""){
+                            //     return true;
+                            // }else 
+                            // if(tool.name.toLowerCase().includes(settings.search.toLowerCase())){
+                            //     return tool;
+                            // }
+                            if (filter.pricing_type !== "Default") {
+                                if (tool.pricing_type !== filter.pricing_type) {
+                                    isValid = false;
+                                }
+                            }
 
-                                <img src={tool.coverImage} alt={tool.name} />
-                           
-                           
-                            <div className="SearchResultContent">
-                                <h3>{tool.name}</h3>
-                                <p>{tool.description}</p>
-                                
-                            <div className="Meta">
-                         
-                                    <span className="pricing_type">{tool.pricing_type}</span>
-                            </div>
-                            <div className="d-flex g-1">
+                            if (filter.categories.length === 0) {
+                                isValid = true;
+                            }
+                            // check if filter categories matches with tool categories
+                            if (filter.categories.some(category => tool.categories.findIndex(toolCategory => toolCategory.slug === category) >= 0)) {
+                                isValid = true;
+                            }
 
-                                <Link href={`/directory/${tool.slug}`} className="CheckOut">
-                                  Check it Out
-                                  <TbArrowBigRightLine />
-                                    </Link>
-                                <Link href={tool.link} className="TryOut" target="_blank">
-                                Try it Out
-                                  <TbExternalLink />
-                                    </Link>
-                            </div>
+                            return isValid;
+                        }).map((tool) => {
+
+                            return (<div className="SearchResult" key={tool._id}>
+
+                                <Image
+                                    width={350}
+                                    height={400}
+                                    src={tool.coverImage} alt={tool.name} />
+
+
+                                <div className="SearchResultContent">
+                                    <h3>{tool.name}</h3>
+                                    <p>{tool.description}</p>
+
+                                    <div className="Meta">
+
+                                        <span className="pricing_type">{tool.pricing_type}</span>
+                                    </div>
+                                    <div className="d-flex g-1">
+
+                                        <Link href={`/directory/${tool.slug}`} className="CheckOut">
+                                            Check it Out
+                                            <TbArrowBigRightLine />
+                                        </Link>
+                                        <Link href={tool.link} className="TryOut" target="_blank">
+                                            Try it Out
+                                            <TbExternalLink />
+                                        </Link>
+                                    </div>
                                 </div>
 
-                    </div>)
-                    })}
-                    {loading ? <Wobble/> : null}
-                </DirectoryPageSearchResults>
+                            </div>)
+                        })}
+                    </DirectoryPageSearchResults>
 
-                <DirectoryPageSearchFilters>
-                    {categories.map((category) =>{
-                        return (
-                            <div className="Filter" key={category._id}>
-                                <h3>{category.name}</h3>
-                            </div>
-                        )
-                    }) }
-                </DirectoryPageSearchFilters>
+                    <DirectoryPageSearchFilters>
+                        {categories.map((category) => {
+                            return (
+                                <FormElement className="Filter" key={category._id}>
+                                    <CheckBox
+                                        checked={filter.categories.includes(category.slug)}
+                                        onChange={() => {
 
-</div>
+                                            if (filter.categories.includes(category.slug)) {
+                                                setFilter({
+                                                    ...filter,
+                                                    categories: filter.categories.filter((cat) => cat !== category.slug)
+                                                })
+                                            } else {
+                                                setFilter({
+                                                    ...filter,
+                                                    categories: [...filter.categories, category.slug]
+                                                })
+                                            }
+
+                                        }
+                                        }
+                                        id={category.slug}
+
+                                    />
+                                    <Label htmlFor={category.slug}>{category.name}</Label>
+                                </FormElement>
+                            )
+                        })}
+                    </DirectoryPageSearchFilters>
+
+                </div>
 
             </DirectoryPageContainer>
-            <Footer socialMedia={SocialMedia}/>
+            <Footer socialMedia={SocialMedia} />
         </>
     )
 }
@@ -219,7 +316,7 @@ export async function getServerSideProps(context) {
         message: string
     };
 
-    
+
     const response = await axios.post(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/public-tools/all`, {
 
     }, {
@@ -231,19 +328,19 @@ export async function getServerSideProps(context) {
             data.type = "success";
             data.message = response.data.message;
             console.log(response.data)
-           return response;
+            return response;
         })
         .catch(error => {
             data.type = "error";
             data.message = error.message;
             return error.response
         });
-  
 
-        return {
-        props: { 
-            tools:response.data.tools as PublicToolType[],
-            categories : response.data.categories,
+
+    return {
+        props: {
+            tools: response.data.tools as PublicToolType[],
+            categories: response.data.categories,
             data,
         },
 
