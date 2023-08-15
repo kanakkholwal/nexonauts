9eca590b71d061e09221bb19736701de4edb822a
Kanak <kkupgrader.fs@gmail.com>
2023-08-15 22:14:25 +0530

    ..

--- pages/dashboard/admin/blog/index.tsx
+++ pages/dashboard/admin/blog/index.tsx
@@ -1,23 +1,38 @@
 import { GetSessionParams, getSession } from "next-auth/react";
 import DashboardPage from "components/dashboard-page";
 import Head from "next/head";
+
 import axios from "axios";
 import toast,{ Toaster } from "react-hot-toast";
 import { sessionType } from "@/src/types/session";
 import { SessionUserType, UserType } from "@/src/types/user";
 import styled from "styled-components";
+import dbConnect from "lib/dbConnect";
+import Post,{Comment} from "models/post";
 
-export default function UserPage({  user }: {  user: UserType }) {
+export default function UserPage({ 
+     user,
+     data
+
+     }: {  
+        user: UserType,
+        data:{
+            posts:number,
+            comments:number
+        }
+     }) {
     // Render post...
    
     return (<>
         <Head>
-            <title>Blog Stats| {process.env.NEXT_PUBLIC_WEBSITE_NAME}</title>
+            <title>Blog Stats | {process.env.NEXT_PUBLIC_WEBSITE_NAME || "Nexonauts"}</title>
         </Head>
         <DashboardPage
             user={user}
             headerChildren={<span className="h6">Blog Stats</span>}>
-            
+       Posts : {data.posts} <br/>
+         Comments : {data.comments} 
+
         </DashboardPage>
         <Toaster />
     </>
@@ -46,14 +61,27 @@ export async function getServerSideProps(context: GetSessionParams) {
             }
         }
     }
+    await dbConnect();
+    const [posts,comments] = await Promise.allSettled([
+        Post.countDocuments({}).exec(),
+        Comment.countDocuments({}).exec()
+    ]);
 
-    
+
+        
         return {
             props: {
                 user: session.user,
+                data:{
+                    posts: posts.status === 'rejected' ? 0 : posts.value,
+                    comments: comments.status === 'rejected' ? 0 : comments.value
+                }
             }
         }
+        
     
+
+
     
 
 
