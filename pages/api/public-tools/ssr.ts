b6f2729de09c30529e1531970fe1eeabbe1286e8
Kanak <kkupgrader.fs@gmail.com>
2023-09-01 19:02:52 +0530

    directory ui update

--- pages/api/public-tools/ssr.ts
+++ pages/api/public-tools/ssr.ts
@@ -6,15 +6,15 @@ import { hasSsrMiddleware } from 'middleware/checkUser';
 import PublicTool from 'models/public-tool';
 
 export default nextConnect(handler)
-    .get(async (req: NextApiRequest, res: NextApiResponse,next) => {
-        try{
-            await hasSsrMiddleware(req,res,next);
+    .get(async (req: NextApiRequest, res: NextApiResponse, next) => {
+        try {
+            await hasSsrMiddleware(req, res, next);
             await dbConnect();
-              
+
             // Perform the search with pagination and sorting
-            const tools = await PublicTool.find({status:"published" || "approved"});
-           
-           return  res.json({
+            const tools = await PublicTool.find({ status: "published" || "approved" });
+
+            return res.json({
                 success: true,
                 message: "Tool fetched successfully",
                 tools
@@ -30,30 +30,30 @@ export default nextConnect(handler)
 
 
     })
-    .post(async (req: NextApiRequest, res: NextApiResponse,next) => {
-        try{
-            await hasSsrMiddleware(req,res,next);
+    .post(async (req: NextApiRequest, res: NextApiResponse, next) => {
+        try {
+            await hasSsrMiddleware(req, res, next);
 
             await dbConnect();
-            const {slug} = req.body;
-            const tool = await PublicTool.findOne({slug,status:"published" || "approved"});
-            if(!tool){
+            const { slug } = req.body;
+            const tool = await PublicTool.findOne({ slug, status: "published" || "approved" });
+            if (!tool) {
                 return res.status(404).json({
-                    success:false,
-                    message:"Tool not found",
-                    tool:null
+                    success: false,
+                    message: "Tool not found",
+                    tool: null
                 })
             }
             // run a query to find similiar tools based on category and tags and return them
-            const similarTools = await PublicTool.find({category:tool.category,tags:tool.tags,status:"published" || "approved"})
-            .sort({createdAt:-1})
-            .limit(8)
+            const similarTools = await PublicTool.find({ category: tool.category, tags: tool.tags, status: "published" || "approved" })
+                .sort({ createdAt: -1 })
+                .limit(5)
 
             return res.json({
                 success: true,
                 message: "Tool fetched successfully",
                 tool,
-                related:similarTools
+                related: similarTools
             });
 
         }
@@ -62,4 +62,4 @@ export default nextConnect(handler)
             return res.status(500).json({ success: false, message: error.message ?? "Internal Server Error" });
         }
     })
- 
+
