b92cf680d523c456739149519af2f9e3315164b7
Kanak <kkupgrader.fs@gmail.com>
2023-10-15 20:35:32 +0530

    nexonauts: new apps search page layout

--- @/components/ui/button.tsx
+++ @/components/ui/button.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hero-button-gradient rounded-lg",
   {
     variants: {
       variant: {
@@ -54,3 +54,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
--- src/lib/apps/utils.ts
+++ src/lib/apps/utils.ts
@@ -1,5 +1,28 @@
+import { CgWorkAlt } from 'react-icons/cg';
+import { FcSalesPerformance } from 'react-icons/fc';
+import { GoPerson } from 'react-icons/go';
+import { LiaMoneyBillWaveSolid } from 'react-icons/lia';
+import { MdBusiness, MdOutlineAutoGraph } from 'react-icons/md';
+import { PiStudentFill } from 'react-icons/pi';
+import { TbHealthRecognition, TbSocial } from 'react-icons/tb';
 import type { App } from 'types/app';
 
+export const CATEGORIES = [
+    { label: "Education", value: "education",Icon:PiStudentFill },
+    { label: "Personal", value: "personal" ,Icon:GoPerson},
+    { label: "Career", value: "career",Icon:CgWorkAlt },
+    { label: "Business", value: "business",Icon:MdBusiness },
+    { label: "Health", value: "health",Icon:TbHealthRecognition },
+    // { label: "Lifestyle", value: "lifestyle" },
+    { label: "Sales & Marketing", value: "sales_and_marketing" ,Icon:FcSalesPerformance},
+    { label: "Finance", value: "finance" ,Icon:LiaMoneyBillWaveSolid},
+    { label: "Productivity", value: "productivity" ,Icon:MdOutlineAutoGraph},
+    { label: "Social", value: "social",Icon:TbSocial },
+] as {
+    label: string;
+    value: string;
+    Icon:React.ElementType;
+}[];
 
 export function getUsageData(apps: App[], userId: string) {
     const today = new Date().toISOString().split('T')[0]; // Get today's date
@@ -57,17 +80,3 @@ export function getMostUsedApp(apps: App[], userId: string) {
 }
 
 
-export const CATEGORIES = [
-{ label: "Education", value: "education" },
-{ label: "Personal", value: "personal" },
-{ label: "Career", value: "career" },
-{ label: "Business", value: "business" },
-{ label: "Health", value: "health" },
-{ label: "Fitness", value: "fitness" },
-{ label: "Lifestyle", value: "lifestyle" },
-{ label: "Sales & Marketing", value: "sales_and_marketing" },
-{ label: "Finance", value: "finance" },
-{ label: "Productivity", value: "productivity" },
-{ label: "Social", value: "social" },
-{ label: "Entertainment", value: "entertainment" }
-]
