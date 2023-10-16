ee3de84a7ad75dab20318f8d19e0febdefb0fe19
Kanak <kkupgrader.fs@gmail.com>
2023-10-16 15:58:32 +0530

    improved navigation

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -35,8 +35,8 @@ export default class MyDocument extends Document {
         return (
             <Html lang='en'>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700&family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
                     <NextScript async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6988693445063744" ></NextScript>
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700&family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
                     <link href='https://fonts.googleapis.com/css?family=Fira+Code:wght@300,400,500,600&display=swap' rel='stylesheet' />
                 </Head>
                 <body>
