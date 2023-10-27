ecc0d68d612f3385eb8537cbf3c18b5c847b2a42
Kanak <kkupgrader.fs@gmail.com>
2023-10-27 11:32:19 +0530

    first nexo deployement

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-import { NavBar, AllArticles, HomePageHero} from "components/blog";
 import axios from "axios";
-import {registerView} from "lib/analytics";
-import Footer from "components/footer";
+import { AllArticles, HomePageHero, NavBar } from "components/blog";
 import Button from "components/buttons";
+import Footer from "components/footer";
+import { registerView } from "lib/analytics";
+import dbConnect from 'lib/dbConnect';
+import Post from 'models/post';
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import { BiDownArrowAlt } from "react-icons/bi";
 import { CgSpinnerTwo } from "react-icons/cg";
-import Head from "next/head";
-
 
 export default function BlogHomePage({ initialPosts }) {
   const [posts, setPosts] = useState(initialPosts);
@@ -18,10 +19,10 @@ export default function BlogHomePage({ initialPosts }) {
   const fetchMorePosts = async () => {
     try {
       setLoading(true);
-      const response = await axios.get(`/api/posts/all?page=${currentPage + 1}`,{
+      const response = await axios.get(`/api/posts/all?page=${currentPage + 1}`, {
         headers: {
-            "x-authorization": `Bearer ${process.env.NEXT_AUTH_SECRET}`,
-            'Content-Type': 'application/json'
+          "x-authorization": `Bearer ${process.env.NEXT_AUTH_SECRET}`,
+          'Content-Type': 'application/json'
         }
       });
       const newPosts = response.data.posts;
@@ -37,7 +38,7 @@ export default function BlogHomePage({ initialPosts }) {
   };
 
   useEffect(() => {
-    registerView({ title: "K K UPGRADER BLOG", type: "article", slug: "/blog",postId: null })
+    registerView({ title: "K K UPGRADER BLOG", type: "article", slug: "/blog", postId: null })
   }, []);
 
   return (
@@ -54,45 +55,45 @@ export default function BlogHomePage({ initialPosts }) {
       <AllArticles posts={posts} />
       <div className="d-flex justify-content-around align-items-center">
         {currentPage === totalPages ? <>No More Posts !!!</>
-        :<Button nature="blog-theme" fill={true} onClick={fetchMorePosts}>
-          {loading ? <> <CgSpinnerTwo style={{
-            animation: "spinner 1s linear infinite",
-          }}/> Loading...</> : <><BiDownArrowAlt /> Load More </>}
-        </Button>}
-        </div>
+          : <Button nature="blog-theme" fill={true} onClick={fetchMorePosts}>
+            {loading ? <> <CgSpinnerTwo style={{
+              animation: "spinner 1s linear infinite",
+            }} /> Loading...</> : <><BiDownArrowAlt /> Load More </>}
+          </Button>}
+      </div>
       <Footer only="true" />
-   
+
     </div>
   );
 }
 
 export async function getServerSideProps() {
+  await dbConnect();
 
-    const response = await axios({
-        url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/posts/all`,
-        method: 'get',
-        headers: {
-            "x-authorization": `Bearer ${process.env.NEXT_AUTH_SECRET}`,
-            'Content-Type': 'application/json'
-        }
-    }).then((res) => {
-        return res;
-    }).catch((err) => {
-        return err.response;
-    });
-    if (response.data.success === true && response.data.posts) {
+  const page = 1; // Default to page 1 if not provided
+  const limit = 10; // Default to 10 posts per page
+  const skip = (page - 1) * limit;
 
-        return {
-            props: {
-                initialPosts:response.data.posts,
-            }
-        }
+  const posts = await Post.find({ state: 'published' })
+  .populate('author')
+  .select('title description slug labels image author createdAt publishedAt comments')
+  .populate('analytics')
+  .skip(skip)
+  .limit(limit)
+  .exec();
+
+
+
+  return {
+    props: {
+      initialPosts:JSON.parse(JSON.stringify(posts)) || [],
     }
-    else if (response.data.success === false) {
-        // not found, return
-        return {
-            notFound: true,
-        }
-    }
-  
+  }
+
+  // // not found, return
+  // return {
+  //   notFound: true,
+  // }
+
+
 }
