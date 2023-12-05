f77c05c10144737d18d536a4120006b359255fba
Kanak <kkupgrader.fs@gmail.com>
2023-12-05 14:29:34 +0530

    marketplace-> ux improvement

--- src/layouts/marketplace/navbar.tsx
+++ src/layouts/marketplace/navbar.tsx
@@ -8,6 +8,22 @@ import { NAME } from "src/constants/marketplace";
 import { SessionUserType } from "src/types/user";
 
 import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
     NavigationMenu,
     NavigationMenuContent,
     NavigationMenuItem,
@@ -18,7 +34,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 
-
+import { signOut } from "next-auth/react";
 
 
 const product_types = [
@@ -26,22 +42,22 @@ const product_types = [
     { title: 'UI Kits', href: '/marketplace/explore?type=uikit', description: "UI Kits are collections of components that can be used to build a website." },
     { title: 'E-Books', href: '/marketplace/explore?type=ebook', description: "E-Books are digital books that can be read on your computer, tablet, or phone." },
     { title: 'Plugins', href: '/marketplace/explore?type=plugin', description: "Plugins are software components that add specific features to an existing program." },
-    { title: 'Themes', href: '/marketplace/explore?type=theme',description:"Themes are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Graphics', href: '/marketplace/explore?type=graphic',description:"Graphics are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Fonts', href: '/marketplace/explore?type=font',description:"Fonts are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Themes', href: '/marketplace/explore?type=theme', description: "Themes are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Graphics', href: '/marketplace/explore?type=graphic', description: "Graphics are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Fonts', href: '/marketplace/explore?type=font', description: "Fonts are pre-built websites that you can use as a starting point for your own website." },
     // { title: 'Videos', href: '/marketplace/explore?type=video',description:"Videos are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Courses', href: '/marketplace/explore?type=course',description:"Courses are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Courses', href: '/marketplace/explore?type=course', description: "Courses are pre-built websites that you can use as a starting point for your own website." },
     // { title: 'Audio', href: '/marketplace/explore?type=audio' ,description:"Audio are pre-built websites that you can use as a starting point for your own website."},
     // { title: '3D Assets', href: '/marketplace/explore?type=3d' ,description:"3D Assets are pre-built websites that you can use as a starting point for your own website."},
     // { title: 'Tools', href: '/marketplace/explore?type=tool',description:"Tools are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Mobile Apps', href: '/marketplace/explore?type=mobileapp' ,description:"Mobile Apps are pre-built websites that you can use as a starting point for your own website."},
-    { title: 'Desktop Apps', href: '/marketplace/explore?type=desktopapp',description:"Desktop Apps are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Mobile Apps', href: '/marketplace/explore?type=mobileapp', description: "Mobile Apps are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Desktop Apps', href: '/marketplace/explore?type=desktopapp', description: "Desktop Apps are pre-built websites that you can use as a starting point for your own website." },
     // { title: 'SaaS', href: '/marketplace/explore?type=saas',description:"SaaS are pre-built websites that you can use as a starting point for your own website." },
     // { title: 'Games', href: '/marketplace/explore?type=game' ,description:"Games are pre-built websites that you can use as a starting point for your own website."},
     // { title: 'Websites', href: '/marketplace/explore?type=website' ,description:"Websites are pre-built websites that you can use as a starting point for your own website."},
     // { title: 'Jobs', href: '/marketplace/explore?type=job',description:"Jobs are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Services', href: '/marketplace/explore?type=service',description:"Services are pre-built websites that you can use as a starting point for your own website." },
-    { title: 'Other', href: '/marketplace/explore?type=other',description:"Other are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Services', href: '/marketplace/explore?type=service', description: "Services are pre-built websites that you can use as a starting point for your own website." },
+    { title: 'Other', href: '/marketplace/explore?type=other', description: "Other are pre-built websites that you can use as a starting point for your own website." },
 ];
 
 export function Navbar({ user, title }: {
@@ -62,25 +78,80 @@ export function Navbar({ user, title }: {
         </div>
 
         <NavigationMenuBar />
-      
+
         <button role="button"
-                onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    // setOpen((open) => !open)
+            onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                // setOpen((open) => !open)
 
-                }}
-                className="ml-auto mr-2 inline-flex w-full items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-slate-100 shadow-sm hover:bg-slate-200 h-9 px-4 py-2  rounded-xl relative justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-96">
-                <span className="hidden lg:inline-flex">Search templates, ui kits, e-books, and more...</span>
-                <span className="inline-flex lg:hidden">Search...</span>
+            }}
+            className="ml-auto mr-2 inline-flex w-full items-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-slate-100 shadow-sm hover:bg-slate-200 h-9 px-4 py-2  rounded-xl relative justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-96">
+            <span className="hidden lg:inline-flex">Search templates, ui kits, e-books, and more...</span>
+            <span className="inline-flex lg:hidden">Search...</span>
 
-                <kbd className="pointer-events-none absolute right-1.5 top-[50%] translate-y-[-50%] hidden h-5 select-none items-center gap-1 rounded border bg-muted text-slate-600 px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-                    <span className="text-xs">⌘</span>J
-                </kbd>
-            </button>
+            <kbd className="pointer-events-none absolute right-1.5 top-[50%] translate-y-[-50%] hidden h-5 select-none items-center gap-1 rounded border bg-muted text-slate-600 px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+                <span className="text-xs">⌘</span>J
+            </kbd>
+        </button>
         {user ? <div className="flex items-center space-x-2 px-3 py-1.5 shrink-0">
             <span className="text-slate-500 text-md mr-2 font-semibold sm:inline-block hidden">Hi, {user.name}</span>
-            <Image src={user.profileURL} height={80} width={80} alt="avatar" className="w-8 h-8 rounded-full shadow-sm" />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="relative h-8 w-8 rounded-full ring-transparent focus:ring-transparent">
+                        <Avatar className="h-8 w-8">
+                            <AvatarImage src={user.profileURL} height={80} width={80} alt="avatar" className="w-8 h-8 rounded-full shadow-sm" />
+                            <AvatarFallback>SC</AvatarFallback>
+                        </Avatar>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuLabel className="font-normal">
+                        <div className="flex flex-col space-y-1">
+                            <p className="text-sm font-medium leading-none">{user.name}</p>
+                            <p className="text-xs leading-none text-muted-foreground">
+                                @{user.username}
+                            </p>
+                        </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem asChild>
+                            <Link href={`/marketplace/profiles/${user.username}`}>
+                                Profile
+                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href={`/marketplace/my/products`}>
+                                My Products
+                                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href={`/marketplace/my/seller-dashboard`}>
+                                Seller Dashboard
+                                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                            </Link>
+                        </DropdownMenuItem>
+
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild >
+                        <button 
+                        className="w-full"
+                        onClick={(e) => {
+                        signOut({
+                            callbackUrl: `/marketplace`
+                        })
+                    }}>
+
+                        Log out
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div> : <div className="flex items-center space-x-2 px-3 py-1.5">
             <Link href="/login" passHref>
                 <a className="text-slate-500 text-md mr-2 font-semibold sm:inline-block hidden">Login</a>
