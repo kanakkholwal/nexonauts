8ee88635c29d79714a85183f7b2fa6a0f4648b9d
Kanak <kkupgrader.fs@gmail.com>
2023-12-05 14:29:34 +0530

    marketplace-> ux improvement

--- pages/marketplace/my/account.tsx
+++ pages/marketplace/my/account.tsx
@@ -1,7 +1,17 @@
 import { getSession } from 'next-auth/react';
 import { NextSeo } from 'next-seo';
-import { FULL_DESCRIPTION, NAME, TITLE } from 'src/constants/marketplace';
+import { FULL_DESCRIPTION, TITLE } from 'src/constants/marketplace';
 import Wrapper from 'src/layouts/marketplace';
+
+
+import { Button } from '@/components/ui/button';
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs";
+import { PlusCircleIcon } from "lucide-react";
 export default function Market({ user }) {
     return (<>
         <NextSeo
@@ -9,10 +19,39 @@ export default function Market({ user }) {
             description={FULL_DESCRIPTION} />
         <Wrapper user={user}>
             <section className="w-full p-3  mt-4 ">
-                <h1 className="text-2xl font-bold text-slate-900 flex items-center gap-2">
-                    Your Account
-                </h1>
-
+                <Tabs defaultValue="music" className="h-full space-y-6">
+                    <div className="space-between flex items-center">
+                        <TabsList className='bg-slate-200 text-slate-700'>
+                            <TabsTrigger value="account" className="relative ">
+                                Account
+                            </TabsTrigger>
+                            <TabsTrigger value="Products">
+                                Products
+                            </TabsTrigger>
+                            <TabsTrigger value="live" disabled>
+                                Settings
+                            </TabsTrigger>
+                        </TabsList>
+                        <div className="ml-auto mr-4">
+                            <Button>
+                                <PlusCircleIcon className="mr-2 h-4 w-4" />
+                                Add Products
+                            </Button>
+                        </div>
+                    </div>
+                    <TabsContent
+                        value="account"
+                        className="border-none p-0 outline-none"
+                    >
+                        Account
+                    </TabsContent>
+                    <TabsContent
+                        value="Products"
+                        className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                    >
+                        Products
+                    </TabsContent>
+                </Tabs>
             </section>
 
 
--- pages/marketplace/my/products/index.tsx
+++ pages/marketplace/my/products/index.tsx
@@ -1,18 +1,65 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import { getSession } from 'next-auth/react';
 import { NextSeo } from 'next-seo';
+import Link from 'next/link';
+import React from 'react';
 import { FULL_DESCRIPTION, TITLE } from 'src/constants/marketplace';
 import Wrapper from 'src/layouts/marketplace';
+import ProductCard, { ProductCardSkeleton } from 'src/layouts/marketplace/product-card';
+
+const cache = new Map();
+
+const skeleton = new Array(4).fill(0);
+
 export default function Market({ user }) {
+
+    const [products, setProducts] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(null);
+
+    React.useEffect(() => {
+        if (cache.has("my-products")) {
+            setProducts(cache.get("my-products"));
+        } else {
+            fetch(`/api/marketplace/trending/${user._id}`)
+                .then(res => res.json())
+                .then(res => {
+                    cache.set("my-products", res);
+                    setProducts(res);
+                }).catch(err => {
+                    setError(err);
+                }).finally(() => {
+                    // setLoading(false);
+                })
+        }
+    }, [user])
+
     return (<>
         <NextSeo
             title={TITLE}
             description={FULL_DESCRIPTION} />
         <Wrapper user={user}>
             <section className="w-full p-3  mt-4 ">
-                <h1 className="text-2xl font-bold text-slate-900 flex items-center gap-2">
-                    My Products
-                </h1>
+                <div className='flex justify-between items-center bg-white p-4 rounded-lg'>
+                    <h1 className="text-2xl font-bold text-slate-900 flex items-center gap-2">
+                        My Products
+                    </h1>
+                    <Button asChild>
+                        <Link href='/marketplace/my/products/new'>
+                            <Plus className='mr-2 w-4 h-6' />Add Product
+                        </Link>
+                    </Button>
+                </div>
+                <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 mt-4 md:px-8 ">
+            {loading && skeleton.map((_, i) => {
+                return (<ProductCardSkeleton key={i} />)
+            })}
 
+            {products.map((product, i) => {
+                return (<ProductCard key={i} product={product}/>)
+            })}
+        </div>
             </section>
 
 
