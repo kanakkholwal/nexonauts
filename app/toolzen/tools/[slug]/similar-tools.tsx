13628c21ca7555e0fea5b542225bb4520c10ed42
Kanak <kkupgrader.fs@gmail.com>
2024-02-03 19:13:34 +0530

    Update import statement for PublicToolTypeWithId in similar-tools.tsx

--- app/toolzen/tools/[slug]/similar-tools.tsx
+++ app/toolzen/tools/[slug]/similar-tools.tsx
@@ -1,6 +1,6 @@
-import { PublicToolTypeWithId } from 'app/toolzen/models/tool';
 import { Zap } from 'lucide-react';
 import Link from 'next/link';
+import { PublicToolTypeWithId } from 'src/models/tool';
 export default function SimilarTools({ tools }: { tools: PublicToolTypeWithId[] }) {
 
     
