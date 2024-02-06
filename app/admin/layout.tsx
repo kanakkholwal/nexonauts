c7555f9075f4e26a2bedee9777f4bc2384a1b3c8
Kanak <kkupgrader.fs@gmail.com>
2024-02-07 00:59:38 +0530

    admin dashboard started and ui improvements on dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,29 +1,31 @@
 import { authOptions } from "app/api/auth/[...nextauth]/options";
+import Navbar from "app/dashboard/components/navbar";
+import SideBar from "app/dashboard/components/sidenav";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import Navbar from "./components/navbar";
-import SideBar from "./components/sidenav";
 
-export const metadata :Metadata = {
-    title: "Dashboard - NexoNauts",
-    description: "Dashboard for your account",
+export const metadata: Metadata = {
+    title: "Admin Dashboard - NexoNauts",
+    description: "Admin Dashboard ",
 }
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
     const session = await getServerSession(authOptions);
     console.log(session)
-    if (!(session && session.user)) return redirect("/login")
+    if (!(session && session.user)) return redirect("/login");
+    if (session.user.role !== "admin") return redirect("/dashboard");
 
     return (<>
-        <div className="flex h-full min-h-screen selection:bg-primary/10 selection:text-primary dark:bg-gray-800/80 bg-slate-200/80 z-0">
-            <SideBar user={session.user}  />
+        <div className="flex h-full min-h-screen selection:bg-primary/10 selection:text-primary dark:bg-gray-900 bg-slate-200/80 z-0">
+            <SideBar user={session.user} />
             <div className="lg:pl-80 flex flex-col flex-1 w-full relative z-0">
                 <Navbar user={session.user} />
                 <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20 -z-[1]">
-                <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700" />
-                <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600" />
-            </div>
+                    <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700" />
+                    <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600" />
+                </div>
+
                 <main className="content p-2 md:p-4 z-2">
                     {children}
                 </main>
