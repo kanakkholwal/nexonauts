4a2a535b9b57db6449fea2d8c88f8f971793315a
Kanak <kkupgrader.fs@gmail.com>
2024-02-10 02:24:45 +0530

    Landing page improvements

--- app/layouts/navbar.tsx
+++ app/layouts/navbar.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -66,9 +67,9 @@ export default function Navbar() {
     <div className="mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0">
       <div className="relative flex flex-wrap items-center justify-between gap-6 lg:gap-0 lg:py-4">
         <div className="relative z-20 flex w-full justify-between md:px-0 lg:w-max">
-          <a href="/" aria-label="logo" className="flex items-center space-x-2">
-            <img className="h-10 dark:invert" src="/assets/logo.svg" alt="logo" />
-          </a>
+          <Link href="/" aria-label="logo" className="flex items-center space-x-2">
+            <Image height={40} width={280} className="h-10 dark:invert" src="/assets/logo.svg" alt="logo" />
+          </Link>
           <button aria-label="humburger" id="hamburger" className="relative -mr-6 p-6 lg:hidden"
             onClick={() => setOpen(!open)}
           >
@@ -80,8 +81,8 @@ export default function Navbar() {
         <div id="layer" aria-hidden={open ? "false" : "true"} className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-gray-900/70 lg:hidden" />
         <div id="navlinks" className={"invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-auto lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:peer-checked:translate-y-0 dark:lg:bg-transparent" + (open ? " !visible !scale-100 !opacity-100 !lg:translate-y-0" : "")}>
           <div className="text-gray-600 dark:text-gray-300 lg:pr-4">
-            <NavigationMenu className="flex-col sm:flex-row">
-              <NavigationMenuList>
+            <NavigationMenu>
+              <NavigationMenuList className="flex-col sm:flex-row">
                 <NavigationMenuItem>
                   <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
                   <NavigationMenuContent>
