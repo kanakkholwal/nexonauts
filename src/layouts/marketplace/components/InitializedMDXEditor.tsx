93722bb44a7c74e8d6ab2deef32c6aa17bec9e64
Kanak <kkupgrader.fs@gmail.com>
2024-02-15 22:21:20 +0530

    Refactor MDXEditor imports and remove unused code

--- src/layouts/marketplace/components/InitializedMDXEditor.tsx
+++ src/layouts/marketplace/components/InitializedMDXEditor.tsx
@@ -1,53 +1,53 @@
 "use client"
 // InitializedMDXEditor.tsx
-import {
-  MDXEditor,
-  UndoRedo,
-  headingsPlugin,
-  imagePlugin,
-  linkDialogPlugin,
-  linkPlugin,
-  listsPlugin,
-  markdownShortcutPlugin,
-  quotePlugin,
-  tablePlugin,
-  thematicBreakPlugin,
-  toolbarPlugin,
-  type MDXEditorMethods,
-  type MDXEditorProps
-} from "@mdxeditor/editor";
-import type { ForwardedRef } from "react";
+// import {
+//   MDXEditor,
+//   UndoRedo,
+//   headingsPlugin,
+//   imagePlugin,
+//   linkDialogPlugin,
+//   linkPlugin,
+//   listsPlugin,
+//   markdownShortcutPlugin,
+//   quotePlugin,
+//   tablePlugin,
+//   thematicBreakPlugin,
+//   toolbarPlugin,
+//   type MDXEditorMethods,
+//   type MDXEditorProps
+// } from "@mdxeditor/editor";
 
 
 
 
 
 // Only import this to the next file
-export default function InitializedMDXEditor({
-  editorRef,
-  ...props
-}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
-  return (
-    <MDXEditor
-      plugins={[
-        // Example Plugin Usage
-        linkPlugin(),
-    linkDialogPlugin(),
-    imagePlugin(),
-        headingsPlugin(),
-        tablePlugin(),
-        listsPlugin(),
-        quotePlugin(),
-        thematicBreakPlugin(),
-        markdownShortcutPlugin(),
+// export default function InitializedMDXEditor({
+//   editorRef,
+//   ...props
+// }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+//   return null;
+//   return (
+//     <MDXEditor
+//       plugins={[
+//         // Example Plugin Usage
+//         linkPlugin(),
+//     linkDialogPlugin(),
+//     imagePlugin(),
+//         headingsPlugin(),
+//         tablePlugin(),
+//         listsPlugin(),
+//         quotePlugin(),
+//         thematicBreakPlugin(),
+//         markdownShortcutPlugin(),
 
 
-        toolbarPlugin({ toolbarContents: () => <div className="w-full flex items-center bg-slate-100">
-              <UndoRedo />
-        </div>}),
-      ]}
-      {...props}
-      ref={editorRef}
-    />
-  );
-}
+//         toolbarPlugin({ toolbarContents: () => <div className="w-full flex items-center bg-slate-100">
+//               <UndoRedo />
+//         </div>}),
+//       ]}
+//       {...props}
+//       ref={editorRef}
+//     />
+//   );
+// }
--- src/layouts/marketplace/components/editor.tsx
+++ src/layouts/marketplace/components/editor.tsx
@@ -1,29 +1,29 @@
 "use client"
 // MDXEditor.tsx
-import {
-  type MDXEditorMethods,
-  type MDXEditorProps,
-} from "@mdxeditor/editor";
+// import {
+//   type MDXEditorMethods,
+//   type MDXEditorProps,
+// } from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
 import { forwardRef } from "react";
 import { CgSpinner } from "react-icons/cg";
 
 // This is the only place InitializedMDXEditor is imported directly.
-const ForwardRefEditor = dynamic(() => import("./InitializedMDXEditor"), {
-  // Make sure we turn SSR off
-  ssr: false,
-  loading: () => <div className="w-full p-5 h-[20rem] rounded-xl bg-slate-100 flex items-center justify-center">
+// const ForwardRefEditor = dynamic(() => import("./InitializedMDXEditor"), {
+//   // Make sure we turn SSR off
+//   ssr: false,
+//   loading: () => <div className="w-full p-5 h-[20rem] rounded-xl bg-slate-100 flex items-center justify-center">
 
-        <CgSpinner className="animate-spin  h-10 w-10" size={32} />
+//         <CgSpinner className="animate-spin  h-10 w-10" size={32} />
 
-  </div>,
-});
+//   </div>,
+// });
 
 // This is what is imported by other components. Pre-initialized with plugins, and ready
 // to accept other props, including a ref.
-export const Editor = forwardRef<MDXEditorMethods, MDXEditorProps>(
-  (props, ref) => <ForwardRefEditor {...props} editorRef={ref} />,
-);
+// export const Editor = forwardRef<MDXEditorMethods, MDXEditorProps>(
+//   (props, ref) => <ForwardRefEditor {...props} editorRef={ref} />,
+// );
 
 // TS complains without the following line
-Editor.displayName = "MDXEditor";
+// Editor.displayName = "MDXEditor";
