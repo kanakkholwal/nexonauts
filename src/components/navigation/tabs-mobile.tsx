e61206f966d8204476aee6204b9c7d39357dc101
Kanak <kkupgrader.fs@gmail.com>
2024-03-18 15:01:15 +0530

    Refactor marketplace page and mobile category navigation

--- src/components/navigation/tabs-mobile.tsx
+++ src/components/navigation/tabs-mobile.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-// Note: This component is used to filter resources by category
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { GoPlus } from "react-icons/go";
-
 interface Props {
     categories: readonly string[];
     key?: string;
@@ -27,45 +34,34 @@ export default function CatgeoryNavigationMobile({ categories, key = "category" 
     };
 
     return (
-        <div className="fixed bottom-0 left-0 right-0  border-border dark:border-slate-100/10 backdrop-blur border-opacity-15 shadow p-4 flex justify-center gap-x-2 gap-y-2 sm:hidden z-50">
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className={`py-4 px-4 flex gap-x-1 font-bold border bg-white dark:bg-black/70 border-dim-gray rounded-md hover:border-text transition-all text-sm items-center w-full justify-between ${activeCategory === "" ? " text-accent" : ""
-                    }`}
-            >
-                {activeCategory === "" ? "All" : activeCategory}
-                <span>
-                    <GoPlus />
-                </span>
-            </button>
-            {isOpen && (
-                <div className="absolute bottom-16 left-0 right-0 border
-                border-border dark:border-slate-100/10 backdrop-blur-xl border-opacity-15 shadow bg-slate-200/80 dark:bg-slate-800/40 p-4 m-4 flex rounded-md flex-col gap-y-2 ">
-                    <button
-                        onClick={() => handleCategoryChange("")}
-                        className={`py-4 px-4 flex gap-x-1 font-medium rounded border border-transparent hover:border-dim-gray transition-all text-sm ${activeCategory === "" ? "bg-accent text-bg" : " bg-bg text-accent"
-                            }`}
-                    >
-                        All
-                    </button>
-                    {categories.map((item: string) => {
-                        return (
-                            <button
-                                key={item}
-                                onClick={() => handleCategoryChange(item)}
-                                className={`py-4 px-4 flex gap-x-1 font-medium  rounded border border-transparent hover:border-dim-gray transition-all text-sm relative 
-                                ${activeCategory === item
-                                        ? "text-bg bg-accent"
-                                        : " bg-none text-slate-300"
-                                    }`}
-                            >
-                                <span className="text-sm">{item}</span>
+        <div className={cn(
+            "fixed bottom-0 left-0 right-0  p-4 flex justify-center gap-x-2 gap-y-2 sm:hidden z-50",
+            " bg-white/30 dark:bg-slate-100/5 border-border dark:border-slate-100/10 backdrop-blur-xl border-opacity-15 shadow"
+        )}>
 
-                            </button>
-                        );
-                    })}
-                </div>
-            )}
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline"
+                        className="text-sm items-center w-full justify-between"
+                    >
+                        {activeCategory === "" ? "All" : activeCategory}
+                        <span>
+                            <GoPlus />
+                        </span>
+
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-full max-w-[24rem]">
+                    <DropdownMenuRadioGroup value={activeCategory} onValueChange={handleCategoryChange}>
+                        {categories.map((item: string) => {
+                            return (
+                                <DropdownMenuRadioItem key={item} value={item}>{item}</DropdownMenuRadioItem>
+                            )
+                        })}
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 }
