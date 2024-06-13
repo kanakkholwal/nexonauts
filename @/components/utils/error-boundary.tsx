3bf4d059db3bdf17f4c216db1b22a9b314c18d72
Kanak <kanakkholwal@gmail.com>
2024-06-13 15:41:46 +0530

    chore:some improvements

---
+++ @/components/utils/error-boundary.tsx
@@ -0,0 +1,88 @@
+"use client"
+import React, { ReactElement, ReactNode, Suspense, useEffect, useState } from 'react';
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+    fallback: ReactElement;
+}
+
+export const ErrorBoundary: React.FC<ErrorBoundaryProps> = ({ children, fallback }) => {
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        const errorHandler = (ev: ErrorEvent) => {
+            setError(ev.error);
+        };
+
+        window.addEventListener('error', errorHandler);
+
+        return () => {
+            window.removeEventListener('error', errorHandler);
+        };
+    }, []);
+
+    useEffect(() => {
+        const unhandledRejectionHandler = (ev: PromiseRejectionEvent) => {
+            setError(ev.reason);
+        };
+
+        window.addEventListener('unhandledrejection', unhandledRejectionHandler);
+
+        return () => {
+            window.removeEventListener('unhandledrejection', unhandledRejectionHandler);
+        };
+    }, []);
+
+    if (error) {
+        return fallback;
+    }
+
+    return <>{children}</>;
+};
+
+interface ErrorBoundaryWithSuspenseProps {
+    children: ReactNode;
+    fallback: ReactElement;
+    loadingFallback: ReactElement;
+}
+export const ErrorBoundaryWithSuspense: React.FC<ErrorBoundaryWithSuspenseProps> = ({
+    children,
+    fallback,
+    loadingFallback,
+}) => {
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        const errorHandler = (ev: ErrorEvent) => {
+            setError(ev.error);
+        };
+
+        window.addEventListener('error', errorHandler);
+
+        return () => {
+            window.removeEventListener('error', errorHandler);
+        };
+    }, []);
+
+    useEffect(() => {
+        const unhandledRejectionHandler = (ev: PromiseRejectionEvent) => {
+            setError(ev.reason);
+        };
+
+        window.addEventListener('unhandledrejection', unhandledRejectionHandler);
+
+        return () => {
+            window.removeEventListener('unhandledrejection', unhandledRejectionHandler);
+        };
+    }, []);
+
+    if (error) {
+        return fallback;
+    }
+
+    return (
+        <Suspense fallback={loadingFallback}>
+            <React.Fragment>{children}</React.Fragment>
+        </Suspense>
+    );
+};
