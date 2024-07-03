332c8e790fec47a44a2f324545c1153895ff6660
Kanak <kanakkholwal@gmail.com>
2024-07-03 19:21:46 +0530

    refactor: Update middleware to handle URL redirects and set headers

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const toolPaths = ['/tool-scout/tools/', '/toolbox/tools/', '/toolzen/tools/'];
+  const matchedPath = toolPaths.find(path => pathname.startsWith(path));
+
+  if (matchedPath) {
+    const newUrl = new URL(pathname.replace(matchedPath, '/scout/tools/'), request.url);
+    return NextResponse.redirect(newUrl);
+  }else if (pathname.startsWith('/toolbox?query')) {
+    const newUrl = new URL(pathname.replace('/toolbox?query', '/scout/browse?query'), request.url);
+    return NextResponse.redirect(newUrl);
+  }
+
+  const headers = new Headers(request.headers);
+  const rootUrl = process.env.NEXTAUTH_URL as string;
+  headers.set("x-current-path", rootUrl + pathname);
+  return NextResponse.next({ headers });
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
---
+++ src/types/global-env.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+
+const envVariables = z.object({
+    // Server Side
+    GOOGLE_ID: z.string(),
+    GOOGLE_SECRET: z.string(),
+    //   GOOGLE_CLIENT_ID: z.string(),
+    //   GOOGLE_CLIENT_SECRET: z.string(),
+    NEXTAUTH_URL: z.string(),
+    NEXT_AUTH_SECRET: z.string(),
+    MONGODB_URI: z.string(),
+    NODE_ENV: z.string().default("testing"),
+    GUMROAD_APP_ID: z.string(),
+    GUMROAD_APP_SECRET: z.string(),
+    GUMROAD_ACCESS_TOKEN: z.string(),
+    GITHUB_ID: z.string(),
+    GITHUB_SECRET: z.string(),
+    GUMROAD_API_ENDPOINT: z.string(),
+    CLOUDINARY_CLOUD_NAME: z.string(),
+    CLOUDINARY_API_KEY: z.string(),
+    CLOUDINARY_API_SECRET: z.string(),
+    GEMINI_API_KEY: z.string(),
+    MAIL_EMAIL: z.string(),
+    MAIL_PASSWORD: z.string(),
+    WEBSITE_NAME: z.string(),
+    WEBSITE_DOMAIN: z.string(),
+    WEBSITE_URL: z.string(),
+    JWT_SECRET: z.string(),
+
+    // Client Side
+    NEXT_PUBLIC_WEBSITE_NAME: z.string(),
+    NEXT_PUBLIC_WEBSITE_URL: z.string(),
+    NEXT_PUBLIC_ENV: z.string(),
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
+    NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: z.string(),
+    NEXT_PUBLIC_CLOUDINARY_FOLDER: z.string(),
+    NEXT_PUBLIC_GUMROAD_APP_ID: z.string(),
+});
+
+envVariables.parse(process.env);
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv extends z.infer<typeof envVariables> { }
+    }
+}
