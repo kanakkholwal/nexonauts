768511592eecb9673365d4381f5ba52ddc8b5cb8
Kanak <kanakkholwal@gmail.com>
2024-07-04 00:06:40 +0530

    refactor: Update marketplace page layout and styles

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -45,7 +45,7 @@ export default async function Page() {
                   </Link>
                 </Button>
               </div>
-              <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <div className="grid gap-4 px-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {result.products.map((product) => {
                   return (
                     <Link
