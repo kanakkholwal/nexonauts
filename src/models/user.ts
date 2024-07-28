6b4b114fd5a83a5f6c7cd559c5c3033c4b0e534d
Kanak <kanakkholwal@gmail.com>
2024-07-28 22:06:42 +0530

    Staging (#108)
    * refactor: hashing issue
    
    * refactor:Improving dev experience
    
    * minor patch
    
    * new-model:base
    
    * patch:bump version to latest nexo-mdx
    
    * refactor
    
    * chore: Update nexo-mdx dependency to version 0.8.0
    
    ---------
    
    Signed-off-by: Kanak <kanakkholwal@gmail.com>

--- src/models/user.ts
+++ src/models/user.ts
@@ -196,7 +196,6 @@ userSchema.pre<User>("save", async function (next) {
 
   try {
     const hash = await bcrypt.hash(this.password, 10);
-
     this.password = hash;
     next();
   } catch (err) {
