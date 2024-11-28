1d8b59de57f97db3c7e80e78e68e28e2f50ba838
Kanak <kanakkholwal@gmail.com>
2024-11-28 11:14:43 +0530

    refactor: Update post-card component to use createdAt instead of updatedAt for date display

--- app/blog/authors/[username]/page.tsx
+++ app/blog/authors/[username]/page.tsx
@@ -1,5 +1,5 @@
 
-
+import { getPostsByAuthor } from 'src/lib/blog/actions'
 
 interface PageProps {
     params:Promise<{
@@ -9,9 +9,14 @@ interface PageProps {
 
 export default async function AuthorPage(props:PageProps){
     const {username} = await props.params
+    const {profile, posts} = await getPostsByAuthor(username)
 
+    if(!profile){
+        return <div>Author not found</div>
+    }
 
     return <>
+    
     
     </>
 }
---
+++ src/models/index.ts
@@ -0,0 +1,10 @@
+import User from "./user";
+import Message from "./message";
+import Post from "./post";
+import Product from "./product";
+import Profile from "./profile";
+import PublicTool from "./tool";
+import Rating from "./tool-rating";
+
+
+export { User,Profile, Message, Post, Product, PublicTool, Rating };
