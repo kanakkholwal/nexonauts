0029e2262ed88e8c5a145f23d719601ce7d45d0d
Kanak <kanakkholwal@gmail.com>
2024-12-13 14:21:15 +0530

    refactor: Remove HtmlToJsxTool and bgRemovalTool from the dev tools collection

--- app/blog/articles/[slug]/components/comments.tsx
+++ app/blog/articles/[slug]/components/comments.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import Giscus from "@giscus/react";
 import { useTheme } from "next-themes";
 
 export function CommentsSection() {
   const { theme } = useTheme();
-  return (
-    <Giscus
-      id="comments"
-      repo="kanakkholwal/nexonauts"
-      repoId="R_kgDOKgo8BA"
-      category="Show and tell"
-      categoryId="DIC_kwDOKgo8BM4CgEoh"
-      mapping="pathname"
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme={theme}
-      lang="en"
-      loading="lazy"
-    />
-  );
+
+  return null
+
 }
--- app/dev-tools/(tools)/collection/bg-remover/tool.tsx
+++ app/dev-tools/(tools)/collection/bg-remover/tool.tsx
@@ -1,27 +1,46 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { removeBackground } from "@imgly/background-removal";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+
+
 
 export default function ToolComponent() {
-    const [image, setImage] = useState<File|null>(null)
+    const [image, setImage] = useState<File | null>(null)
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const removeBackgroundInstance = useRef<any>(null)
 
     const handleRemoval = async () => {
         if (!image) return
 
         const imageUri = URL.createObjectURL(image)
+        const removeBackground = removeBackgroundInstance.current
+        if(!removeBackgroundInstance.current) {
+            alert("Remove background library not loaded")
+            return
+        }
         const result = await removeBackground(imageUri)
         console.log(result)
 
 
     }
+    useEffect(() => {
+        async function loadLibrary() {
+            if (window === undefined) return
+            // Dynamically import the library
+            removeBackgroundInstance.current = await import("@imgly/background-removal");
+            console.log("Library loaded")
+        }
+        loadLibrary();
+    }, []);
 
     return <>
         <div>
             <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Label htmlFor="picture">Picture</Label>
-                <Input id="picture" type="file" 
+                <Input id="picture" type="file"
                     onChange={(e) => setImage(e.target.files?.[0] || null)}
                     multiple={false}
                     accept="image/*"
@@ -31,7 +50,7 @@ export default function ToolComponent() {
         <div>
             <h4 className="text-lg font-semibold">Settings</h4>
             <div className="grid w-full max-w-sm items-center gap-1.5">
-                
+                <Button onClick={handleRemoval}>Remove Background</Button>
             </div>
         </div>
     </>
