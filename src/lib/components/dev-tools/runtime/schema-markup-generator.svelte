1e5a8e74f140b5a7e76920f90e8f6c01e60c5484
kanakkholwal <kanakkholwal@gmail.com>
2026-05-09 14:52:04 +0530

    migration to SvelteKit 2

--- src/lib/components/dev-tools/runtime/schema-markup-generator.svelte
+++ src/lib/components/dev-tools/runtime/schema-markup-generator.svelte
@@ -1,3 +1,5 @@
+<!-- @migration-task Error while migrating Svelte code: Can only bind to an Identifier or MemberExpression or a `{get, set}` pair
+https://svelte.dev/e/bind_invalid_expression -->
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button";
 	import { Input } from "$lib/components/ui/input";
